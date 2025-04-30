9dd98f2165ae871f22eb3134485c455375e43687
Unmey Mahaddalkar <unmey08@gmail.com>
2025-04-29 21:35:04 -0700

    deployment changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/users": {
+        target: "http://localhost:3200",
+      },
+    },
+  },
 });
